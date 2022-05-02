6eaaa1a35bdf4a06a5f817dfb5a581cd5155b221
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-02 08:41:28 -0300

    aa

---
+++ parouimpar.c
@@ -0,0 +1,26 @@
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+    int n;
+
+    printf("insira um numero positivo");
+    scanf("%d", &n);
+
+    if (n % 2 == 0){
+        printf("numero par");
+
+
+
+}
+
+else{
+    printf("numero impar");
+}
+
+
+
+}
