139353cf2e08b4e209167c9737d4db1dd5eb5b7a
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-02 08:28:24 -0300

    aa

---
+++ areacirculo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+
+{
+    float raio,a;
+
+    printf("entre com o raio");
+    scanf("%f",&raio);
+
+    a = (3.14)*pow(raio,2);
+
+    printf("\n a area e: %f", a);
+
+
+}
